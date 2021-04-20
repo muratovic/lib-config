2b20770ff3427495b829efe5f86fe1933aedc97e
Murat Öztürk <muratozturk1987@gmail.com>
2021-04-20 11:28:36 +0300

    Mahmud isleri geri alindi

--- modules/RTC/RTCUtils.js
+++ modules/RTC/RTCUtils.js
@@ -338,7 +338,9 @@ function getConstraints(um, options = {}) {
             // which, in the case a users has multiple monitors, leads to them being shared all
             // at once. However we tested with chromeMediaSourceId present and it seems to be
             // working properly and also takes care of the previously mentioned issue.
-            constraints.audio = { echoCancellation: true };
+            constraints.audio = { mandatory: {
+                chromeMediaSource: constraints.video.mandatory.chromeMediaSource
+            } };
         }
     }
 
@@ -918,48 +920,17 @@ class RTCUtils extends Listenable {
         logger.info('Get media constraints', JSON.stringify(constraints));
 
         return new Promise((resolve, reject) => {
-            if (constraints.audio && constraints.video) {
-                const audioConstraints = {
-                    audio: constraints.audio
-                };
-
-                const videoConstraints = {
-                    video: constraints.video
-                };
-
-                Promise.all([
-                    navigator.mediaDevices.getUserMedia(audioConstraints),
-                    navigator.mediaDevices.getUserMedia(videoConstraints)
-                ])
-                .then(([ audioStream, videoStream ]) => {
-                    const combinedStream = new MediaStream([
-                        ...videoStream.getVideoTracks(),
-                        ...audioStream.getAudioTracks()
-                    ]);
-
-                    updateGrantedPermissions(um, combinedStream);
-                    resolve(combinedStream);
-                })
-                .catch(error => {
-                    logger.warn(`Failed to get access to local media. ${error} ${JSON.stringify(constraints)}`);
-                    updateGrantedPermissions(um, undefined);
-                    reject(new JitsiTrackError(error, constraints, um));
-                });
-            } else {
-                navigator.mediaDevices.getUserMedia(constraints)
-                .then(stream => {
-                    logger.log('onUserMediaSuccess');
-                    updateGrantedPermissions(um, stream);
-                    resolve(stream);
-                })
-                .catch(error => {
-                    logger.warn(`Failed to get access to local media. ${error} ${JSON.stringify(constraints)}`);
-                    updateGrantedPermissions(um, undefined);
-                    reject(new JitsiTrackError(error, constraints, um));
-                });
-            }
-
-
+            navigator.mediaDevices.getUserMedia(constraints)
+            .then(stream => {
+                logger.log('onUserMediaSuccess');
+                updateGrantedPermissions(um, stream);
+                resolve(stream);
+            })
+            .catch(error => {
+                logger.warn(`Failed to get access to local media. ${error} ${JSON.stringify(constraints)}`);
+                updateGrantedPermissions(um, undefined);
+                reject(new JitsiTrackError(error, constraints, um));
+            });
         });
     }
 
